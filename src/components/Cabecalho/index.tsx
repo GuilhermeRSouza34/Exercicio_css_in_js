9721cde65dcf804866c0102844de2d6ffbd14576
Guilherme <guilhermersouza34@gmail.com>
2024-06-26 10:49:08 -0300

    Exercico_CSS_JS

---
+++ src/components/Cabecalho/index.tsx
@@ -0,0 +1,9 @@
+import { HeaderModule } from './style'
+
+const Cabecalho = () => (
+  <HeaderModule>
+    <h1>EBAC Jobs</h1>
+  </HeaderModule>
+)
+
+export default Cabecalho
---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,13 @@
+import { HeroForm, HeroTitle } from './style'
+
+const Hero = () => (
+  <HeroForm>
+    <div className="container">
+      <HeroTitle>
+        As melhores vagas para tecnologia, design e artes visuais.
+      </HeroTitle>
+    </div>
+  </HeroForm>
+)
+
+export default Hero
